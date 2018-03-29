21f3e3cca15bfd659a63c694be51c3310b287935
kammann123 <lucaskammann@hotmail.com>
2018-03-29 09:31:13 -0300

    plan...

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* Constantes del juego de la vida */
+#define ROWS 10           // Cantidad de filas del mundo 2D
+#define COLS 10           // Cantidad de columnas del mundo 2D
+#define CELL_ALIVE  0xFF  // Cuando una celula esta viva
+#define CELL_DEAD   0x00  // Cuando una celula esta muerta
+#define CELL_BORN   0xF0  // Cuando una celula acaba de nacer
+#define CELL_DYING  0x0F  // Cuando una celula acaba de morir
+
+#define ERROR    -1       // Como se devuelven errores
+#define NOT_ERROR 0
+
+#define TRUE  1
+#define FALSE 0
+
+/* Funciones control de cambio entre generaciones */
+unsigned char cellBorn(char cells[ROWS][COLS], int row, int col);
+unsigned char cellSurvive(char cells[ROWS][COLS], int row, int col);
+unsigned char cellDies(char cells[ROWS][COLS], int row, int col);
+void cellsStateUpdate(char actual[ROWS][COLS], char new[ROWS][COLS], char changes[ROWS][COLS]);
+
+/* Funciones de interaccion con el usuario IO */
+void printScreen(char cells[ROWS][COLS]);
+void clearScreen(void);
+unsigned char getInput(char *str);
+unsigned int getNewCells(char cells[ROWS][COLS]);
+
+/* Funciones generales */
+unsigned int createNewCells(char cells[ROWS][COLS]);
+void copyArray(char *from, char *to, int length);
